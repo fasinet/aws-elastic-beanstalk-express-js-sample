84127d06cd0420051f70e3e4fb2bed63e610cafb
fasinet <fasifaiz799@gmail.com>
2020-12-08 19:40:12 +1100

    full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Fasi!'));
+app.get('/', (req, res) => res.send('Fasi! now it has changed'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
